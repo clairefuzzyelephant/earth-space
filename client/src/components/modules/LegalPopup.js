94e4b726eea45ceafa4b369476cb6f6bb7ea4caf
Claire Cheng <clairecheng88@gmail.com>
2021-03-30 18:40:50 -0400

    popup and accept working with submission failure guidance

--- client/src/components/modules/LegalPopup.js
+++ client/src/components/modules/LegalPopup.js
@@ -3,14 +3,31 @@ import React from "react";
 import "./LegalPopup.css";
 
 function Popup (props) {
-    console.log("HEYY")
+  console.log("HEYY")
+  const { acceptFunction, cancelFunction } = props;
+
   return (
     <>
         <div className="Popup-background">
         </div>
         <div className="Popup-container">
-            What's upPPPPPp
+            <h1>Legal Terms and Conditions</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit. Mauris a diam maecenas sed enim ut sem viverra. Ultricies leo integer malesuada nunc. Lectus nulla at volutpat diam ut venenatis tellus. Massa enim nec dui nunc mattis. Cras adipiscing enim eu turpis. Massa tincidunt nunc pulvinar sapien. Amet nisl purus in mollis nunc. Risus nullam eget felis eget nunc lobortis mattis aliquam faucibus. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Ut tristique et egestas quis ipsum. Facilisis leo vel fringilla est ullamcorper. Bibendum enim facilisis gravida neque convallis a cras semper.
+
+            In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Quam vulputate dignissim suspendisse in est ante in nibh. Ultrices dui sapien eget mi proin sed. Euismod lacinia at quis risus sed vulputate. Velit ut tortor pretium viverra suspendisse potenti. At augue eget arcu dictum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer. Massa vitae tortor condimentum lacinia quis vel eros. Sed augue lacus viverra vitae congue eu. Viverra accumsan in nisl nisi. Massa id neque aliquam vestibulum. Lacus luctus accumsan tortor posuere ac ut consequat. Cras sed felis eget velit aliquet. Pellentesque massa placerat duis ultricies. Tortor posuere ac ut consequat semper viverra nam. In pellentesque massa placerat duis ultricies lacus sed. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Malesuada fames ac turpis egestas integer eget. Bibendum ut tristique et egestas quis ipsum suspendisse. Vitae semper quis lectus nulla at volutpat diam.
+
+
+
+            <div className="Popup-buttonGroup">
+                <div className="Popup-cancelButton" onClick={cancelFunction}>
+                    Cancel
+                </div>
+                <div className="Popup-acceptButton" onClick={acceptFunction}>
+                    Accept
+                </div>
+            </div>
         </div>
+        
     </>
   );
 }
--- client/src/components/pages/Submit.js
+++ client/src/components/pages/Submit.js
@@ -1,6 +1,7 @@
 import React, { useState, useMemo, useEffect } from "react";
 import Select from 'react-select';
 import countryList from 'react-select-country-list';
+import { CountryDropdown } from 'react-country-region-selector';
 
 import LegalPopup from "../modules/LegalPopup.js";
 
@@ -19,39 +20,75 @@ function Submit(props) {
     const [countryVal, setCountryVal] = useState("");
     const [message, setMessage] = useState("");
 
-    const [checked, setChecked] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
     const [showPopup, setShowPopup] = useState(false);
+    const [acceptedTerms, setAcceptedTerms] = useState(false);
+    const [displayWarning, setDisplayWarning] = useState(false);
 
-    const options = useMemo(() => countryList().getData(), []);
+    const [issues, setIssues] = useState([]);
+
+    const issueList = ["- Terms and conditions not accepted", "- Legal name not entered", "- Country not selected", "- Empty message"];
+    const issuesOccur = {0: false, 1: false, 2: false, 3: false};
+    
 
     const handleSubmit = () => {
         const body = {legalName: legalName, englishName: englishName, country: countryVal.value, message: message};
-        post("/api/submitMessage", body).then((result) => {
-            if (result !== null) {
-                console.log("Success!");
-                setLegalName("");
-                setEnglishName("");
-                setCountryVal("");
-                setMessage("");
-            }
-        })
-    }
-
-    const togglePopup = () => {
-        if (!checked) {
-            console.log("toggling");
-            setShowPopup(true);
-            setChecked(true);
+        if (acceptedTerms && legalName !== "" && countryVal !== "" && message !== "") {
+            post("/api/submitMessage", body).then((result) => {
+                if (result !== null) {
+                    console.log("Success!");
+                    setLegalName("");
+                    setEnglishName("");
+                    setCountryVal("");
+                    setMessage("");
+                }
+            })
         }
         else {
-            setShowPopup(false);
-            setChecked(false);
+            if (!acceptedTerms) {
+                issuesOccur[0] = true;
+            }
+            if (legalName == "" || !legalName) {
+                issuesOccur[1] = true;
+            }
+            if (countryVal == "" || !countryVal) {
+                issuesOccur[2] = true;
+            }
+            if (message == "" || !message) {
+                issuesOccur[3] = true;
+            }
+            setIssues(issueList.filter(function(i) {
+                return issuesOccur[issueList.indexOf(i)]
+            }))
+            setDisplayWarning(true);
+            console.log(issues);
+        }
+            
+    }
+
+    const toggleCheckbox = () => {
+        console.log("value of state " + isChecked);
+        if (!isChecked) {
+            console.log("toggling checkbox");
+            setShowPopup(true);
+        }
+        else { //already checked
+            setIsChecked(false);
+            setAcceptedTerms(false);
         }
     }
 
-    useEffect(() => {
-        console.log("toggling popup!!!")
-    }, [showPopup])
+    const acceptTerms = () => {
+        setAcceptedTerms(true);
+        setIsChecked(true);
+        setShowPopup(false);
+        setDisplayWarning(false);
+    }
+
+    const cancelTerms = () => {
+        setShowPopup(false);
+    }
+
 
     return (
         <>
@@ -65,26 +102,38 @@ function Submit(props) {
                     <input className="Submit-smallField" placeholder="Legal Name" value={legalName} onChange={e => setLegalName(e.target.value)} />
                     <input className="Submit-smallField" placeholder="English Name (if applicable)" value={englishName} onChange={e => setEnglishName(e.target.value)}/>
                     <div>
-                        <Select className="Submit-dropdown" isSearchable={false} placeholder="No Country Selected" value={countryVal} options={options} onChange={e => setCountryVal(e)} />
+                        <CountryDropdown className="Submit-dropdown" showDefaultOption={true} defaultOptionLabel="No Country Selected" value={countryVal} onChange={e => setCountryVal(e)} />
                     </div>
                     <div className="Submit-legalCheckbox">
-                        <div><input type="checkbox" value={checked} onChange={() => togglePopup()} /></div>
+                        <div><input type="checkbox" checked={isChecked} onChange={() => toggleCheckbox()} /></div>
                         <div>I accept the legal terms and conditions.</div>
                     </div>
                 </div>
                 <div className="Submit-inputInfoRight">
-                    <textarea className="Submit-largeField" placeholder="Type your message here..." value={message} onChange={e => setMessage(e.target.value)}/>
+                    <textarea className="Submit-largeField" placeholder="Type your message here... (Max 200 characters)" maxlength={200} value={message} onChange={e => setMessage(e.target.value)}/>
+                    <div className="Submit-submitButton" onClick={() => handleSubmit()}>
+                        Send
+                    </div>
+                    
+                    {displayWarning ? 
+                        <div className="Submit-notAcceptedWarning">
+                            Please resolve the following issues before continuing: 
+                            <br/>
+                            {issues.map((issue) =>  
+                                <>                     
+                                    <p>{issue}</p>
+                                </>
+                            )}
+                        </div> 
+                    : null}
+                    
                 </div>
             </div>
             
-            <div className="Submit-introText">
-            </div>
-            <div className="Submit-submitButton" onClick={() => handleSubmit()}>
-                Send
-            </div>
+            
             </div>
 
-            <div>{showPopup ? <LegalPopup /> : null}</div>
+            <div>{showPopup ? <LegalPopup acceptFunction={() => acceptTerms()} cancelFunction={() => cancelTerms()} /> : null}</div>
         </div>
         </>
     );
