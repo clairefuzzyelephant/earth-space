18bfcdbd90232ea4d8f6507f0bf340bc05675fbf
Claire Cheng <clairecheng88@gmail.com>
2021-04-30 02:35:52 -0400

    hardcoded popup

---
+++ client/src/components/modules/TeamPopup.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./TeamPopup.css";
+
+function TeamPopup (props) {
+  console.log("WHAT UP");
+  const { image, name, role, title, bio, closePopup } = props;
+
+  return (
+    <>
+        <div className="Popup-background">
+        </div>
+        <div className="TeamPopup-container">
+            <div className="TeamPopup-quit" onClick={closePopup}>x</div>
+
+            <img src={image} />
+            <h1>{name}</h1>
+            <p>{role} | {title}</p>
+            <p>{bio}</p>
+        </div>
+        
+        
+    </>
+  );
+}
+
+export default TeamPopup;
--- client/src/components/pages/Team.js
+++ client/src/components/pages/Team.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { Link } from "@reach/router";
+import React, { useEffect, useState } from "react";
+
+import TeamPopup from "../modules/TeamPopup.js";
 
 import "../../utilities.css";
 import "./Team.css";
 
 import claire from "../../../dist/people/claire.jpeg";
 import maya from "../../../dist/people/maya.jpeg";
+import lydia from "../../../dist/people/lydia.jpeg";
 
 function Team(props) {
+
+  const [popupStatus, setPopupStatus] = useState(false);
+
+  const bio = "Maya Nasr is a Lebanese PhD student in the Aerospace Engineering department at MIT working on the Mars Oxygen ISRU Experiment (MOXIE) for NASA JPL’s Mars 2020 mission. She received her Bachelor's and Master's degrees in Aerospace Engineering from MIT in 2018 and 2021 respectively. She is the Policy and Congressional Legislation Leads for the SGAC US Taskforce. She’s also leading the Space Resources, Space Ethics & Human Rights subgroups with the goal of space disarmament and peaceful use of outer space. Her research interests are space systems, international space law, policy and politics, space security and disarmament.";
+
+  useEffect(() => {
+    console.log(popupStatus);
+  }, [popupStatus])
 
   return (
     <>
@@ -17,35 +27,75 @@ function Team(props) {
             Our Team
           </div>
           <div className="Team-section">
+            <div className="Team-member" onClick={e => {setPopupStatus(true); console.log("HII") }}>
+              <img src={maya}/>
+              <div className="Team-member-text">
+                <b>Maya Nasr</b>
+                <p>Project Co-Lead</p>
+                <p>MIT AeroAstro PhD Candidate</p>
+              </div>
+            </div>
             <div className="Team-member">
-              <img src={maya} />
-              <b>Maya Nasr</b>
-              <p>Project Co-Lead | MIT AeroAstro PhD Candidate</p>
+              <img src={lydia} />
+              <div className="Team-member-text">
+                <b>Lihui Lydia Zhang</b>
+                <p>Project Co-Lead</p>
+                <p>MIT Technology Policy 21'</p>
+              </div>
             </div>
             <div className="Team-member">
               <img src={claire} />
-              <b>Lihui Lydia Zhang</b>
-              <p>Project Co-Lead | MIT Technology Policy 21'</p>
+              <div className="Team-member-text">
+                <b>Claire Cheng</b>
+                <p>Web Developer</p>
+                <p>MIT Class of 2022</p>
+              </div>
             </div>
             <div className="Team-member">
               <img src={claire} />
-              <b>Claire Cheng</b>
-              <p>Web Developer | MIT Class of 2022</p>
-            </div>
-            <div className="Team-member">
-              <img src={claire} />
-              <b>Claire Cheng</b>
-              <p>Web Developer</p>
+              <div className="Team-member-text">
+                <b>Jeffrey Hoffman</b>
+                <p>Faculty Supervisor</p>
+                <p>MIT Professor of Aeronautics and Astronautics</p>
+              </div>
             </div>
           </div>
           <div className="Team-title">
             Space Exploration Initiative (SEI) Team
           </div>
           <div className="Team-section">
-            <img src={claire} />
-            <img src={claire} />
-            <img src={claire} />
-            <img src={claire} />
+            <div className="Team-member">
+              <img src={claire} />
+              <div className="Team-member-text">
+                <b>Xin Liu</b>
+                <p>Artistic Advisor</p>
+                <p>Arts Curator at the Space Exploration Initiative at the MIT Media Lab</p>
+              </div>
+            </div>
+            <div className="Team-member">
+              <img src={claire} />
+              <div className="Team-member-text">
+                <b>Sands A Fish</b>
+                <p>Advisor</p>
+                <p>MIT Professor of Aeronautics and Astronautics</p>
+              </div>
+            </div>
+            <div className="Team-member">
+              <img src={claire} />
+              <div className="Team-member-text">
+                <b>Ariel Ekblaw</b>
+                <p>Advisor</p>
+                <p>MIT Professor of Aeronautics and Astronautics</p>
+              </div>
+            </div>
+            <div className="Team-member">
+              <img src={claire} />
+              <div className="Team-member-text">
+                <b>Sean Auffinger</b>
+                <p>Mission Integrator</p>
+                <p>MIT Professor of Aeronautics and Astronautics</p>
+              </div>
+            </div>
           </div>
           <div className="Team-title">
             Additional Collaborators and Legal
@@ -57,10 +107,9 @@ function Team(props) {
             <img src={claire} />
           </div>
 
-          <div className="Home-introText">
-            
-          </div>
         </div>
+        <div>{popupStatus ? <TeamPopup image={maya} name="Maya Nasr" role="Project Co-lead" title="MIT AeroAstro PhD Candidate" bio={bio} closePopup={() => setPopupStatus(false)} /> : null}</div>
+
       </div>
     </>
   );
