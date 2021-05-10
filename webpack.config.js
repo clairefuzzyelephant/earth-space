5585088a029187e9bda35a1c4ab923724ab2fd3a
Claire Cheng <clairecheng88@gmail.com>
2021-05-10 18:12:22 -0400

    trying again

--- webpack.config.js
+++ webpack.config.js
@@ -68,8 +68,6 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.DedupePlugin(), //dedupe similar code 
-    new webpack.optimize.UglifyJsPlugin(), //minify everything
     new webpack.optimize.AggressiveMergingPlugin()//Merge chunks 
   ],
   devServer: {
